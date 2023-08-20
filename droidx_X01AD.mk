9ea533d46436e02f621b7c54dfebb0a3fd1ed56c
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-20 10:31:15 +0700

    X01AD: Set default common to official droidxui

--- droidx_X01AD.mk
+++ droidx_X01AD.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/asus/X01AD/device.mk)
 
 # Inherit some common DroidX-UI stuff.
-$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
+$(call inherit-product, vendor/droidx/config/common.mk)
 
 
 # Set shipping API level
